099e7cbcddeace7c9793bbed6cfc954e8e5a1889
Yunsun <justina7182@gmail.com>
2026-05-06 17:24:47 +0900

    Chore: 불필요 import 제거

--- src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
+++ src/main/java/com/sprint/mission/discodeit/storage/BinaryContentStorage.java
@@ -3,7 +3,6 @@ package com.sprint.mission.discodeit.storage;
 import java.io.InputStream;
 import java.util.UUID;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
 
 public interface BinaryContentStorage {
 
