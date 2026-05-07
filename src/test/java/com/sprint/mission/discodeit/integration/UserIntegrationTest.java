b2cdaf49487c5820875bede97246f745661fbdfe
Yunsun <justina7182@gmail.com>
2026-05-07 18:53:37 +0900

    Chore: 불필요 Import 제거

--- src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
+++ src/test/java/com/sprint/mission/discodeit/integration/UserIntegrationTest.java
@@ -2,7 +2,6 @@ package com.sprint.mission.discodeit.integration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
